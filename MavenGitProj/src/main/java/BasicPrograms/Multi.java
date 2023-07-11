cef8817b4ba5f8230cf83dc5c65a1b4a256d8519
chinnathiru <138884914+chinnathiru@users.noreply.github.com>
2023-07-11 18:36:19 +0530

    Update Multi.java

--- MavenGitProj/src/main/java/BasicPrograms/Multi.java
+++ MavenGitProj/src/main/java/BasicPrograms/Multi.java
@@ -5,9 +5,9 @@ public class Multi {
 	public static void main(String[] args) {
 		int a=10;
 		int b=15;
-		int c=a+b;
+		int c=a*b;
 		System.out.println(c);
-
+		System.out.println("addition"+c);
 	}
 
 }
