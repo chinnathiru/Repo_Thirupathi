7c177c60ba8eed43ef6fd7fc1b191d62dfde3083
chinnathiru <138884914+chinnathiru@users.noreply.github.com>
2023-07-11 17:09:24 +0530

    Create SwapingProgram.java

---
+++ MavenGitProj/src/main/java/BasicPrograms/SwapingProgram.java
@@ -0,0 +1,15 @@
+package BasicPrograms;
+
+public class SwapingProgram {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=15;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("after swapping numner a="+a+"\n b="+b);
+
+	}
+
+}
