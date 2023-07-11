854820f3e69289100ae976969e54b17b324d7b4c
chinnathiru <138884914+chinnathiru@users.noreply.github.com>
2023-07-11 18:01:46 +0530

    Update Sub.java

--- MavenGitProj/src/main/java/BasicPrograms/Sub.java
+++ MavenGitProj/src/main/java/BasicPrograms/Sub.java
@@ -3,7 +3,7 @@ package BasicPrograms;
 public class Sub {
 
 	public static void main(String[] args) {
-	    int a=100;
+	    int a=1000;
 	    int b=15;
 	    int c= a-b;
 	    System.out.println("substraction of c="+c);
