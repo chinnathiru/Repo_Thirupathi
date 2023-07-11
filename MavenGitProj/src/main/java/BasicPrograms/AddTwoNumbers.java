f7a49927f54d56799795d8abe2c6ca2e63d174e0
chinnathiru <138884914+chinnathiru@users.noreply.github.com>
2023-07-11 15:59:09 +0530

    Create AddTwoNumbers.java

---
+++ MavenGitProj/src/main/java/BasicPrograms/AddTwoNumbers.java
@@ -0,0 +1,17 @@
+package BasicPrograms;
+
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a, b values");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println("the value of c ="+c);
+
+	}
+
+}
