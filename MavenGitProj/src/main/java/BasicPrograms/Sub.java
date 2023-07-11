56761927dad3dc0ba182816f4f5269982f6e62b0
chinnathiru <138884914+chinnathiru@users.noreply.github.com>
2023-07-11 17:58:07 +0530

    Create Sub.java

---
+++ MavenGitProj/src/main/java/BasicPrograms/Sub.java
@@ -0,0 +1,12 @@
+package BasicPrograms;
+
+public class Sub {
+
+	public static void main(String[] args) {
+	    int a=100;
+	    int b=15;
+	    int c= a-b;
+	    System.out.println("substraction of c="+c);
+	}
+
+}
