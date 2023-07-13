6d37164b28f3d3b65715e078b5ce9b517b6ca4a6
chinnathiru <138884914+chinnathiru@users.noreply.github.com>
2023-07-13 11:30:30 +0530

    Update FactorCount.java

--- MavenGitProj/src/main/java/BasicPrograms/FactorCount.java
+++ MavenGitProj/src/main/java/BasicPrograms/FactorCount.java
@@ -18,6 +18,17 @@ public class FactorCount {
 			
 		}
 		System.out.println("the factors are="+count);
+		
+		//factorial number......
+//		System.out.println("enter any number");
+//		int na=sc.nextInt()
+	    int fact=1,temp=n;
+		while(n>0)
+		{
+			fact=fact*n;
+			n=n-1;
+		}
+		System.out.println("the factorial value is "+temp +"="+fact);
 	}
 
 }
