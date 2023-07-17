116676c1d0aa77be44a97dff13caa76fdc86ced5
chinnathiru <138884914+chinnathiru@users.noreply.github.com>
2023-07-17 13:13:21 +0530

    Update TableFormat.java

--- MavenGitProj/src/main/java/BasicPrograms/TableFormat.java
+++ MavenGitProj/src/main/java/BasicPrograms/TableFormat.java
@@ -9,7 +9,9 @@ public class TableFormat {
 		for(int i=1;i<=10;i++)
 		{
 			System.out.println(n+"*"+i+"="+(n*i));
-
+			
+            System.out.println(n+"-"+i+"="+(n-i));
+            System.out.println("-----------------------");
 
 
 		}
