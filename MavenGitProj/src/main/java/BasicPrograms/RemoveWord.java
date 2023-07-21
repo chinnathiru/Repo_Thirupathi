8c9bd402d0e53dd4d23275aa9df31ca764e85cec
chinnathiru <138884914+chinnathiru@users.noreply.github.com>
2023-07-21 11:24:04 +0530

    Update RemoveWord.java

--- MavenGitProj/src/main/java/BasicPrograms/RemoveWord.java
+++ MavenGitProj/src/main/java/BasicPrograms/RemoveWord.java
@@ -10,10 +10,10 @@ public class RemoveWord {
 		System.out.println("enter remove word");
 		String str1=sc.next();
 		char ch1[]=str1.toCharArray();
-		int index=0;
+		int count,count1=0,x=0, index=0;
 		for(int i=0;i<ch.length;i++)
 		{
-			int count=0;
+			 count=0;
 			for(int j=0;j<ch1.length;j++)
 			{
 				if(ch[i+j]==ch1[j])
@@ -25,23 +25,34 @@ public class RemoveWord {
 				{
 					break;
 				}
-				index=i+j;
-			}
-			
-			if(count==ch1.length)
-			{
-				
-				for(int k=0;k<ch.length;k++)
+//				if(count==ch1.length)
+//				{
+//					x++;
+//				}
+				if(count==ch1.length)
 				{
-					if(k<i||k>index)
-					{
-						System.out.print(ch[k]);
-					}
-					
+					count1++;
+
+					if(count1>1)
+					{   
+						index=i;	  
+						for(int k=0;k<ch1.length;k++)
+						{
+							for(int l=index;l<ch.length-1;l++)
+							{
+								ch[l]=ch[l+1];
+
+							}  
+						}
 					}
 				}
 			}
+		}	  
 
+		for(int i=0;i<ch.length-((count1*ch1.length)-(ch1.length));i++)
+		{
+			System.out.print(ch[i]);
+		}
 	}
 
 }
