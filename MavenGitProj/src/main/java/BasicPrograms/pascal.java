f1cf2bb8fe42732a5e2b7567289ff296a85fc764
chinnathiru <138884914+chinnathiru@users.noreply.github.com>
2023-07-21 13:18:28 +0530

    Create pascal.java

---
+++ MavenGitProj/src/main/java/BasicPrograms/pascal.java
@@ -0,0 +1,22 @@
+package BasicPrograms;
+
+public class pascal {
+
+	public static void main(String[] args) {
+
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int sp=0;sp<=n-i;sp++)
+			{
+				System.out.print(" ");
+			}
+			
+			
+		
+		
+	}
+
+	}
+
+}
