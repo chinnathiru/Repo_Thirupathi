021551d404ecbdbd6ca87d52f9579bb2bd57fa86
chinnathiru <138884914+chinnathiru@users.noreply.github.com>
2023-08-22 18:57:53 +0530

    update

--- W3school/src/Practices/ProfilePage.java
+++ W3school/src/Practices/ProfilePage.java
@@ -12,6 +12,7 @@ static By framel=By.xpath("//iframe[@id='classic-menu']");
 static By Profilel=By.xpath("//a[text()='Profile']");
 static By nicknamel=By.xpath("//div[text()='No Public @\"nickname\"']");
 static By nickNametextbox=By.xpath("(//input[@class='InputItem_item__Ebrl2'])[4]");
+static By w3scholL=By.xpath("//div[@id='dashboard-button']");
 
 
 public static WebElement frameL()
@@ -37,6 +38,11 @@ public static WebElement nicknameText()
 	WebElement ni=driver.findElement(nickNametextbox);
 	return ni;
 }
+public static WebElement w3school()
+{
+	WebElement w3scl=driver.findElement(w3scholL);
+	return w3scl;
+}
 
 public static void clickprofile() throws InterruptedException
 {
@@ -54,6 +60,18 @@ public static void nickNametext()
 	nicknameText().sendKeys("https://www.w3profile.com/");
 	
 }
+public static WebElement frameL1()
+{
+	WebElement framew=driver.findElement(framel);
+	driver.switchTo().frame(framew);
+	return framew;
+}
+public static void w3schl()
+{
+	 w3school().click();
+}
+
+
 	
 	
 }
